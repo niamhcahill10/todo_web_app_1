9b8ae2d4746afcdb1ea4298632b8ef1dc9bbbe82
niamhcahill <nac1@live.co.uk>
2023-10-12 18:44:08 +0100

    Add configuration files

--- web_todo_app.py
+++ web_todo_app.py
@@ -10,7 +10,10 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
-st.write("This app is to increase your productivity.")
+st.write("This app is to increase your <b>productivity</b>.", unsafe_allow_html=True)
+
+st.text_input(label="todo_input", label_visibility="hidden", placeholder="Add a new todo.",
+              on_change=add_todo, key='new_todo')
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -19,7 +22,3 @@ for index, todo in enumerate(todos):
         tf.write_todos(todos)
         del st.session_state[todo]
         st.rerun()
-
-
-st.text_input(label="todo_input", label_visibility="hidden", placeholder="Add a new todo.",
-              on_change=add_todo, key='new_todo')
