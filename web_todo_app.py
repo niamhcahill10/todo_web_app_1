7c1c60f169c5f88fe586c6e116bde06d115fdfa3
niamhcahill <nac1@live.co.uk>
2023-10-12 14:55:04 +0100

    Initial commit

---
+++ web_todo_app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import todos_functions as tf
+
+todos = tf.get_todos()
+
+def add_todo():
+    new_todo = st.session_state["new_todo"] + "\n"
+    todos.append(new_todo)
+    tf.write_todos(todos)
+
+st.title("My Todo App")
+st.subheader("This is my todo app.")
+st.write("This app is to increase your productivity.")
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        tf.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+
+
+st.text_input(label="todo_input", label_visibility="hidden", placeholder="Add a new todo.",
+              on_change=add_todo, key='new_todo')
